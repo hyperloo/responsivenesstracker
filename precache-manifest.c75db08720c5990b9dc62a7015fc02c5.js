0b21364acad92d034ab4371cdd20d52cb31a8b85
hyperloo <hack.himanshu1024@gmail.com>
2020-10-27 22:12:41 +0530

    Updates

---
+++ precache-manifest.c75db08720c5990b9dc62a7015fc02c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625257aeefaf723109eb4afdd041241b",
+    "url": "/responsivenesstracker/index.html"
+  },
+  {
+    "revision": "6c4b67b3a87185b83e54",
+    "url": "/responsivenesstracker/static/css/main.33c0c8f0.chunk.css"
+  },
+  {
+    "revision": "45bc133fc6104dc1aaf1",
+    "url": "/responsivenesstracker/static/js/2.7f57e4c9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/responsivenesstracker/static/js/2.7f57e4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c4b67b3a87185b83e54",
+    "url": "/responsivenesstracker/static/js/main.e9c4b449.chunk.js"
+  },
+  {
+    "revision": "64d4cfb153ffb7f9a56f",
+    "url": "/responsivenesstracker/static/js/runtime-main.28d9026c.js"
+  },
+  {
+    "revision": "70e7f50d0d9398f9991185dbe5a05d29",
+    "url": "/responsivenesstracker/static/media/Spinner.70e7f50d.gif"
+  },
+  {
+    "revision": "3b8775fa92da5174f9917fc55caf223c",
+    "url": "/responsivenesstracker/static/media/rotate.3b8775fa.svg"
+  }
+]);
